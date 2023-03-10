8094e9cd1dfd983a5c41ce115b0dbf64a3b59ff2
krunal767 <akkimaisuriya@gmail.com>
2023-03-10 20:27:11 +0530

    Initial Commit
    Setup Project

---
+++ Sepia pet/Sepia_petApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sepia_petApp.swift
+//  Sepia pet
+//
+//  Created by mac on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Sepia_petApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
